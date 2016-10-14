48caf2e34126311aa19bfad88594379594dd064c
PJ Fanning <pj.fanning@workday.com>
2016-10-14 23:11:46 +0100

    initial commit

--- src/main/java/com/github/poi/SxssfSavePasswordProtectedXlsx.java
+++ src/main/java/com/github/poi/SxssfSavePasswordProtectedXlsx.java
@@ -23,28 +23,8 @@ import org.apache.poi.xssf.streaming.SXSSFCell;
 import org.apache.poi.xssf.streaming.SXSSFRow;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 
-
-/*
- *  ====================================================================
- *    Licensed to the Apache Software Foundation (ASF) under one or more
- *    contributor license agreements.  See the NOTICE file distributed with
- *    this work for additional information regarding copyright ownership.
- *    The ASF licenses this file to You under the Apache License, Version 2.0
- *    (the "License"); you may not use this file except in compliance with
- *    the License.  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- * ====================================================================
- */
-
 public class SxssfSavePasswordProtectedXlsx {
-    
+
     public static void main(String[] args) {
         try {
             if(args.length != 2) {
@@ -82,5 +62,5 @@ public class SxssfSavePasswordProtectedXlsx {
             t.printStackTrace();
         }
     }
-    
+
 }
